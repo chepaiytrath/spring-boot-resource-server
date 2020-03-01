92aaf98b324033ced06337ef4910a7f65bad7a44
Jatin <jatin.shashoo@gmail.com>
2020-03-02 02:37:25 +0530

    First commit with @EnableAuthorizationServer

---
+++ src/main/java/com/example/springbootresourceserver/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.springbootresourceserver.controller;
+
+import com.example.springbootresourceserver.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/user/getEmployeesList", produces = "application/json")
+    @ResponseBody
+    public List<Employee> getEmployeesList() {
+        List<Employee> employees = new ArrayList<>();
+        Employee emp = new Employee();
+        emp.setEmpId("emp1");
+        emp.setEmpName("emp1");
+        employees.add(emp);
+        return employees;
+    }
+}
